6f8dedda215fa7c6fe31a66a509890435848913d
penny_silv <pennysilv@gmail.com>
2023-11-29 19:20:58 +0500

    update project

--- project.py
+++ project.py
@@ -1,27 +1,38 @@
+from string import punctuation
+
 def open_file(file_name):
     with (open(file_name) as file):
         data = file.read()
-        file.seek(0)
-        lines = file.readlines()
-        return data, lines
+        return data
 
 
-def calculate_lines(lines: list) -> int:
-    number_lines = len(lines)
+def calculate_lines(data: str) -> int:
+    number_lines = 0
+    for i in data:
+        print(i)
+        if i == ('\n'):
+            number_lines += 1
+            print(number_lines)
     return number_lines
 
 
 def calculate_words(data: str) -> int:
+    in_word = False
     number_words = 0
-    words = data.split()
-    for i in range(len(words)):
-        number_words += 1
+    for i in data:
+        if i != ' ' and i != '\n' and in_word == False:
+            number_words += 1
+            in_word = True
+        elif i == ' ' or i == '\n':
+            in_word == False
+            print(number_words)
     return number_words
 
+
 f = input('Введите путь до файла: ')
-text, list_of_strings = open_file(f)
+text = open_file(f)
 print(text)
-print("Количество строк:", calculate_lines(list_of_strings))
+print("Количество строк:", calculate_lines(text))
 print("Количество слов:", calculate_words(text))
 
 
