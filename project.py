36d97013c6e122ec076d132ec96a3bcbd70fcaad
penny_silv <pennysilv@gmail.com>
2023-12-01 18:10:03 +0500

    закончила основной код

--- project.py
+++ project.py
@@ -1,6 +1,7 @@
-from string import punctuation
+from string import punctuation, ascii_letters, digits
 
-def open_file(file_name):
+
+def open_file(file_name, encoding='utf-8'):
     with (open(file_name) as file):
         data = file.read()
         return data
@@ -9,23 +10,24 @@ def open_file(file_name):
 def calculate_lines(data: str) -> int:
     number_lines = 0
     for i in data:
-        print(i)
-        if i == ('\n'):
+        if i == '\n':
             number_lines += 1
-            print(number_lines)
     return number_lines
 
 
 def calculate_words(data: str) -> int:
     in_word = False
     number_words = 0
+    kirill = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+    latin = ascii_letters
+    words = []
     for i in data:
-        if i != ' ' and i != '\n' and in_word == False:
-            number_words += 1
-            in_word = True
-        elif i == ' ' or i == '\n':
-            in_word == False
-            print(number_words)
+        if i != ' ' and i != '\n' and in_word is False and i not in punctuation:
+            if (i in kirill or latin) and i not in digits:
+                in_word = True
+                number_words += 1
+        elif i == ' ' or i == '\n' or i in punctuation:
+            in_word = False
     return number_words
 
 
