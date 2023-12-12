06554bd39a6d69ded545822efda9ef8caace9e87
penny_silv <pennysilv@gmail.com>
2023-12-12 19:52:47 +0500

    пофиксила функции

--- calc_func.py
+++ calc_func.py
@@ -2,11 +2,19 @@ from string import punctuation, ascii_letters, digits
 
 
 def calculate_lines(data: str) -> int:
-    number_lines = 0
-    for i in data:
-        if i == '\n' and i != '\n\n':
-            number_lines += 1
-    return number_lines
+    line_count = 0
+    empty_line = True
+    for ch in data:
+        if ch == '\n' and empty_line:
+            pass
+        elif ch == '\n' and not empty_line:
+            empty_line = True
+            line_count += 1
+        else:
+            empty_line = False
+    if not empty_line:
+        line_count += 1
+    return line_count
 
 
 def calculate_words(data: str) -> int:
@@ -21,3 +29,61 @@ def calculate_words(data: str) -> int:
         elif char == ' ' or char == '\n' or char in punctuation:
             in_word = False
     return number_words
+
+# def calculate_emails(data: str) -> int:
+#     count = 0
+#     has_symbol = True
+#     has_dog = False
+#     while ch < len(data):
+#         while not has_dog and ch in ascii_letters:
+#             ch += 1
+#             has_symbol = True
+#         if ch == '@':
+#             has_dog = True
+#         while ch < len(data) and ch in ascii_letters:
+#
+#
+# def is_symbol(ch: str) -> bool:
+#     return  'a' <= ch <= 'z' or '0' <= ch <= '9'
+#
+#
+# def read_word(data: str) -> (bool, str):
+#     i = 0
+#     while i < len(data) and is_symbol(data[i]):
+#         i += 1
+#     if i == 0:
+#         return False, ""
+#     return True, data[i:]
+#
+# def read_dog(data: str) -> (bool, str):
+#     if len(data) == 0 or data[0] != '@':
+#         return False, ""
+#     return True, data[1:]
+# def is_email(data: str):
+#     i = 0
+#     while i < len(data) and is_symbol(data[i]):
+#         i += 1
+#     if i == 0:
+#         return False
+#
+#     if len(data) == 0 or data[0] != '@':
+#         return False
+#     i += 1
+#
+#     i = 0
+#     while i < len(data) and is_symbol(data[i]):
+#         i += 1
+#     if i == 0:
+#         return False
+#
+# def is_email(data: str) -> (bool, str):
+#     ok, data = read_word(data)
+#     if not ok:
+#         return False
+#     ok, data = read_dog(data)
+#     if not ok:
+#         return False
+#     ok, data = read_word(data)
+#     if not ok:
+#         return False
+#     return True
