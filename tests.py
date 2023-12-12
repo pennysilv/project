36f32a8c6a515f7af88c09e6eda77ef6d47e9a69
penny_silv <pennysilv@gmail.com>
2023-12-12 19:42:45 +0500

    добавила тесты

---
+++ tests.py
@@ -0,0 +1,25 @@
+from calc_func import *
+
+if __name__ == '__main__':
+    assert(calculate_lines("") == 0)
+    assert(calculate_lines("\n") == 0)
+    assert(calculate_lines("a\nb\nc") == 3)
+    assert(calculate_lines("a\nb\n\n") == 2)
+
+    assert(calculate_words("a b c") == 3)
+    assert(calculate_words("fff aaa ccc") == 3)
+    assert(calculate_words("444 aaa.") == 1)
+    assert(calculate_words("арара ляля") == 2)
+
+    assert(calculate_emails("") == 0)
+    assert(calculate_emails("ilya@lispberry.dev") == 1)
+    assert(calculate_emails("ilya@@lispberry.dev") == 0)
+    assert(calculate_emails("@@@") == 1)
+    assert(calculate_emails(" @ ") == 0)
+    assert(calculate_emails(" @") == 0)
+    assert(calculate_emails(",@@") == 1)
+    assert (calculate_emails(",@,") == 1)
+
+
+
+
